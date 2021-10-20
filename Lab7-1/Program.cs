2496ca979a567e30a5771a6c41cd2c6adf3691b9
IliaGavrilov <IliaGavrilov@ILIAGAVRILOV-HP>
2021-10-20 21:49:22 +0300

    Добавьте файлы проекта.

---
+++ Lab7-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер ребра куба = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            CalcVolume(a);
+           // CalcPloshad(a);
+            Console.ReadKey();
+        }
+        static void CalcVolume(int a)
+            
+        {
+            int V = a * a * a;
+            int S = 6 * a * a;
+            Console.WriteLine("Объем куба равен {0}", V);
+            Console.WriteLine("Объем куба равен {0}", S);
+            Console.ReadKey();
+        }
+        
+	    
+    }
+
+}
